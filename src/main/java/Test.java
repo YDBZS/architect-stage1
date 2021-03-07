c64c9c086518da63e50201ec6555afb0c90c4377
GalaxyFever <y908063221@gmail.com>
2021-03-07 10:30:52 +0800

    首次提交

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * 测试测试
+ *
+ * @author 多宝
+ * @since 2021/3/7 10:28
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("你好架构师");
+    }
+
+}
