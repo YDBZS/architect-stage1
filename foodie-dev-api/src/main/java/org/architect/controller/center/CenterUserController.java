2e480dc3b707d1c0989a222b0489def0a5c4379f
vicetone <y908063221@email.com>
2021-05-29 20:06:37 +0800

    学习笔记添加

--- foodie-dev-api/src/main/java/org/architect/controller/center/CenterUserController.java
+++ foodie-dev-api/src/main/java/org/architect/controller/center/CenterUserController.java
@@ -2,6 +2,7 @@ package org.architect.controller.center;
 
 import com.architect.pojo.Users;
 import com.architect.pojo.bo.center.CenterUserBO;
+import com.google.common.collect.Maps;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -12,11 +13,16 @@ import org.architect.service.center.CenterUserService;
 import org.architect.util.CookieUtils;
 import org.architect.util.JsonUtils;
 import org.architect.util.ReturnResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 用户中心接口
@@ -39,15 +45,46 @@ public class CenterUserController {
     @ApiOperation(value = "用户修改账户信息", httpMethod = Constant.INTERFACE_METHOD_POST)
     public ReturnResult update(@RequestParam
                                @ApiParam(name = "userId", value = "用户ID", required = true)
-                                       String userId, @RequestBody CenterUserBO centerUserBO,
+                                       String userId, @RequestBody @Valid CenterUserBO centerUserBO,
+                               BindingResult result,
                                HttpServletRequest request, HttpServletResponse response) {
+
+        // 判断BindingResult是否包含错误的验证信息，如果有则直接return就可以了
+        if (result.hasErrors()) {
+            Map<String, String> map = getErrors(result);
+            return ReturnResult.errorMap(map);
+        }
+
         Users userInfo = centerUserService.updateUserInfo(userId, centerUserBO);
         Users setNull = passprotController.setNull(userInfo);
-        CookieUtils.setCookie(request, response,    "user", JsonUtils.objectToJson(setNull), true);
+        CookieUtils.setCookie(request, response, "user", JsonUtils.objectToJson(setNull), true);
 
         // todo 后续要改，增加令牌tocken，会整合进redis，分布式会话
 
         return ReturnResult.ok();
+    }
+
+    /**
+     * 处理validator错误信息
+     *
+     * @param result 信息
+     * @return java.util.Map<java.lang.String, java.lang.String>
+     * @author 多宝
+     * @since 2021/5/29 19:52
+     */
+    private Map<String, String> getErrors(BindingResult result) {
+
+        Map<String, String> map = Maps.newHashMap();
+
+        List<FieldError> errors = result.getFieldErrors();
+        for (FieldError error : errors) {
+            // 发生验证错误所对应的某一个属性
+            String field = error.getField();
+            // 验证错误信息
+            String message = error.getDefaultMessage();
+            map.put(field, message);
+        }
+        return map;
     }
 
 }
--- foodie-dev-pojo/src/main/java/com/architect/pojo/bo/center/CenterUserBO.java
+++ foodie-dev-pojo/src/main/java/com/architect/pojo/bo/center/CenterUserBO.java
@@ -4,7 +4,9 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.architect.constant.Constant;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 /**
@@ -27,18 +29,25 @@ public class CenterUserBO {
     private String confirmPassword;
 
     @ApiModelProperty(value = "用户昵称", required = Constant.TRUE)
+    @NotBlank(message = "用户昵称不能为空")
+    @Length(max = 12, message = "用户昵称不能超过12位")
     private String nickName;
 
     @ApiModelProperty(value = "真实姓名", required = Constant.TRUE)
+    @Length(max = 12, message = "用户真实姓名不能超过12位")
     private String realName;
 
     @ApiModelProperty(value = "手机号", required = Constant.TRUE)
+    @Pattern(regexp = "^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\\d{8})$", message = "手机号格式不正确")
     private String mobile;
 
     @ApiModelProperty(value = "邮箱地址", required = Constant.TRUE)
+    @Email
     private String email;
 
     @ApiModelProperty(value = "性别", required = Constant.TRUE)
+    @Min(value = 0, message = "性别选择不正确")
+    @Max(value = 2, message = "性别选择不正确")
     private Integer sex;
 
     @ApiModelProperty(value = "生日", required = Constant.TRUE)
